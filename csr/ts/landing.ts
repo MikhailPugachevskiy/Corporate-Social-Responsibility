3e5137e6604cdb7703e280b694aeb1a7af1e95e5
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-23 22:10:09 +0200

    created interface for formular data ts

---
+++ csr/ts/landing.ts
@@ -0,0 +1,12 @@
+// Interface für die Formulardaten
+interface CSRUserData {
+    firstName: string;
+    lastName: string;
+    email: string;
+    education: string;
+    csrRank: string;
+    priorKnowledge: string;
+    unlockedAt: string;
+}
+
+});
